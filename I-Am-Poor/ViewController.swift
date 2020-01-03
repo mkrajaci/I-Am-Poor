b22d28c4466bda96343187f1bfd5e7f1fb9d8a70
Mario Krajacic <krajacic.mario@gmail.com>
2020-01-03 09:13:43 +0100

    Initial Commit

---
+++ I-Am-Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I-Am-Poor
+//
+//  Created by Mario Krajačić on 03/01/2020.
+//  Copyright © 2020 Mario Krajačić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
